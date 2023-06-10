91c2102e0902d0185456373b5d23231be8be0d91
Alessandro Accardo <julius8774@gmail.com>
2023-06-10 15:51:57 +0200

    - added namespace option - added support for annotations - added ability to load a template for the configmap

--- dir2cm.go
+++ dir2cm.go
@@ -11,8 +11,10 @@ import (
 )
 
 type MetaData struct {
-	Name   string            `yaml:"name"`
-	Labels map[string]string `yaml:"labels,omitempty"`
+	Name        string            `yaml:"name"`
+	Namespace   string            `yaml:"namespace,omitempty"`
+	Labels      map[string]string `yaml:"labels,omitempty"`
+	Annotations map[string]string `yaml:"annotations,omitempty"`
 }
 
 type ConfigMap struct {
@@ -22,16 +24,30 @@ type ConfigMap struct {
 	Data       map[string]string `yaml:"data"`
 }
 
-func EmptyConfigMap(name string) *ConfigMap {
+func EmptyConfigMap(name string, namespace string) *ConfigMap {
 	cm := &ConfigMap{
 		ApiVersion: "v1",
 		Kind:       "ConfigMap",
 		Metadata: MetaData{
-			Name: name,
+			Name:      name,
+			Namespace: namespace,
 		},
 		Data: map[string]string{},
 	}
 	return cm
+}
+
+func ParseTemplateYaml(fpath string) (*ConfigMap, error) {
+	contents, err := os.ReadFile(fpath)
+	if err != nil {
+		return nil, err
+	}
+	c := ConfigMap{}
+	err = yaml.Unmarshal([]byte(contents), &c)
+	if err != nil {
+		return nil, err
+	}
+	return &c, nil
 }
 
 // Adds a file
@@ -82,26 +98,58 @@ func main() {
 		panic(err)
 	}
 
-	name := flag.String("name", "my-config", "The ConfigMap Metadata.Name")
-	dir := flag.String("dir", cwd, "The input directory")
+	var name string
+	var dir string
+	var namespace string
+	var template string
+
+	flag.StringVar(&name, "name", "my-config", "The ConfigMap Metadata.Name")
+	flag.StringVar(&name, "n", "my-config", "The ConfigMap Metadata.Name")
+	flag.StringVar(&dir, "dir", cwd, "The input directory")
+	flag.StringVar(&dir, "d", cwd, "The input directory")
+	flag.StringVar(&namespace, "namespace", "", "The ConfigMap Metadata.Namespace")
+	flag.StringVar(&namespace, "ns", "", "The ConfigMap Metadata.Namespace")
+	flag.StringVar(&template, "template", "", "The ConfigMap file to use as a template")
+	flag.StringVar(&template, "t", "", "The ConfigMap file to use as a template")
+
+	usage := fmt.Sprintf(`Usage of ./dir2cm:
+	-d, --dir string
+		  The input directory (default "%s")
+	-n, --name string
+		  The ConfigMap Metadata.Name (default "my-config")
+	-ns, --namespace string
+		  The ConfigMap Metadata.Namespace
+	-t, --template string
+		  The ConfigMap file to use as a template
+	`, cwd)
+	flag.Usage = func() { fmt.Print(usage) }
 
 	flag.Parse()
 
 	//var files []string
-	files, err := os.ReadDir(*dir)
+	files, err := os.ReadDir(dir)
 
 	if err != nil {
 		panic(err)
 	}
 
-	cm := EmptyConfigMap(*name)
+	var cm *ConfigMap
+	if template != "" {
+		cm, err = ParseTemplateYaml(template)
+		if err != nil {
+			log.Warn(err)
+			cm = EmptyConfigMap(name, namespace)
+		}
+	} else {
+		cm = EmptyConfigMap(name, namespace)
+	}
 
 	for _, file := range files {
 		if file.IsDir() {
 			continue
 		}
 
-		fullpath := path.Join(*dir, file.Name())
+		fullpath := path.Join(dir, file.Name())
 		cmf, err := NewConfigMapFile(fullpath)
 		if err != nil {
 			log.Warnf("Problem with file: %s", err)
